fa68f878aa72ae44bf3e11fa9674b6d95c6aa93a
Akash <skytup91@gmail.com>
2025-01-09 04:47:40 +0530

    Updating Iterators

--- stl/vector.cpp
+++ stl/vector.cpp
@@ -1,8 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main(){
+int main()
+{
+    vector<int> a;
+    a.push_back(1);
+    a.emplace_back(2);
 
+    // cout<<a[1];
 
+    vector <pair<int,int>> vec;
+    vec.push_back({1,7});
+    vec.emplace_back(5,2);
+    vec.emplace_back(2,6);
+
+    cout<< vec[2].first;
 }
