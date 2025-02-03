0d57da8e3c2476cdfca3ee13f6e347a16b00ad4b
Akash Vishwakarma <skytup91@gmail.com>
2025-02-03 23:35:50 +0530

    updating the min max problem

--- min-max.cpp
+++ min-max.cpp
@@ -4,25 +4,27 @@ using namespace std;
 
 int getMin(int a[], int n)
 {
-    int min = INT_MAX;
+    int mini = INT_MAX;
     for (int i = 0; i < n; i++)
     {
-        if (a[i] < min)
-        {
-            min = a[i];
-        }
+        mini = min(mini, a[i]);
+        // if (a[i] < min)
+        // {
+        //     min = a[i];
+        // }
     }
-    return min;
+    return mini;
 };
 int getMax(int a[], int n)
 {
-    int max = INT_MIN;
+    int maxi = INT_MIN;
     for (int i = 0; i < n; i++)
     {
-        if (a[i] > max)
-        {
-            max = a[i];
-        }
+        maxi = max(maxi, a[i]);
+        // if (a[i] > max)
+        // {
+        //     max = a[i];
+        // }
     }
     return max;
 };
